fa271c488c6b18a82f47056dafe76d75a68334db
Nathalie Morel <nathalie.s.morel@gmail.com>
2022-03-29 13:26:40 -0300

    Corrigido tipo da variável n de int para long.

--- CalculadoraFatorial.java
+++ CalculadoraFatorial.java
@@ -1,9 +1,9 @@
 
 public class CalculadoraFatorial {
 	public static void main(String[] args) {
-		int n = 4;
-		for (int i = n-1;  i > 1; i = i-1) {
-		   int fatorial = n*i; 
+		long n = 15;
+		for (long i = n-1;  i > 1; i = i-1) {
+		   long fatorial = n*i; 
 		   n = fatorial;	   
 		   }
 		System.out.println("Fatorial = " + n);
